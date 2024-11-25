55efe6cb3775555c2d326b67e2f1cf3f64052008
kartikeya-1112 <metikotikartikeya1112@gmail.com>
2024-11-26 00:19:26 +0530

    changed file locations of start and stop

---
+++ stop_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# List all running Docker containers and stop & remove them one by one
+sudo docker ps | awk 'NR>1 { print $1 }' | while read -r container_id; do
+    echo "Stopping container: $container_id"
+    sudo docker stop "$container_id"
+    echo "Removing container: $container_id"
+    sudo docker rm "$container_id"
+done
+
+echo "All running containers have been stopped and  removed."
+
+
+
