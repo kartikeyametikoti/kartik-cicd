609a6ed8318557446c66c1b382c3a86e034a0904
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-02-15 17:12:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 491085395524.dkr.ecr.us-east-1.amazonaws.com
- 
-docker pull 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
-
-docker run --name=kartik2 -d -p 2000:5000 491085395524.dkr.ecr.us-east-1.amazonaws.com/kartikeya1112/amma:second
-
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 361769586062.dkr.ecr.us-east-1.amazonaws.com
+docker pull 361769586062.dkr.ecr.us-east-1.amazonaws.com/kartik-repo:latest
+docker run --name=kartik2 -d -p 2000:5000 361769586062.dkr.ecr.us-east-1.amazonaws.com/kartik-repo:latest
